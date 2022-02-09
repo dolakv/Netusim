9f9e682b4f91585efe9b687f0b2cf6256091a87e
Vojtěch Dolák <dolakv.05@spst.cz>
2022-02-09 08:59:49 +0100

    A na N

---
+++ AnaN/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnaN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+            double an = Math.Pow(a, n);
+            MessageBox.Show("a na n je" + an);
+        }
+    }
+}
